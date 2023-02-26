09ba79dcee3d4ab3f8b3c50b64c41c4bd1e4abf3
Bartosz Żuk <bartosz.zuk@aiqlabs.pl>
2023-02-26 08:54:57 +0100

    Added recent mean score and steps to DQNTrainer

--- policy/dqn/trainer.py
+++ policy/dqn/trainer.py
@@ -8,17 +8,18 @@ from policy.dqn.config import DQNConfig
 from policy.dqn.replay import ExperienceReplay, Experience
 
 
-def print_episode_statistics(environment: Env | Wrapper, episode: int) -> None:
-    for attribute in ['return_queue', 'length_queue']:
-        if not hasattr(environment, attribute):
-            raise AttributeError(f'Attribute {attribute} not found, maybe RecordEpisodeStatistics missing')
+def compute_mean_score(environment: Env | Wrapper) -> float:
+    if not hasattr(environment, 'return_queue'):
+        raise AttributeError(f'Attribute return_queue not found, maybe RecordEpisodeStatistics missing')
 
-    mean_score = statistics.mean(value.item() for value in environment.return_queue)
-    mean_steps = statistics.mean(value.item() for value in environment.length_queue)
+    return statistics.mean(value.item() for value in environment.return_queue)
 
-    end = '\n' if episode % 100 == 0 else ''
 
-    print(f'\rEpisode {episode} | Mean Score {mean_score:.2f} | Mean Steps {mean_steps:.2f}', end=end)
+def compute_mean_steps(environment: Env | Wrapper) -> float:
+    if not hasattr(environment, 'length_queue'):
+        raise AttributeError(f'Attribute length_queue not found, maybe RecordEpisodeStatistics missing')
+
+    return statistics.mean(value.item() for value in environment.length_queue)
 
 
 class DQNTrainer:
@@ -27,14 +28,38 @@ class DQNTrainer:
         self.agent = agent
         self.config = config
         self.experiences = ExperienceReplay(config.replay_size)
+        self.recent_mean_score = []
+        self.recent_mean_steps = []
 
     def fit(self, environment: Env) -> None:
+        self.agent.train()
+
         for episode in range(1, self.config.episodes + 1):
             self.run_episode(environment, stage='fit')
             self.agent.policy_selection.update()
-            print_episode_statistics(environment, episode)
+
+            mean_score = compute_mean_score(environment)
+            mean_steps = compute_mean_steps(environment)
+
+            self.recent_mean_score.append(mean_score)
+            self.recent_mean_steps.append(mean_steps)
+
+            self.print_episode_statistics(episode)
+
+            if mean_score >= self.config.max_score:
+                print(f'Environment solved - reached max score {self.config.max_score:.2f}! Stopping early ...')
+                break
+
+    def print_episode_statistics(self, episode: int) -> None:
+        mean_score = self.recent_mean_score[-1]
+        mean_steps = self.recent_mean_steps[-1]
+
+        end = '\n' if episode % 100 == 0 else ''
+
+        print(f'\rEpisode {episode} | Mean Score {mean_score:.2f} | Mean Steps {mean_steps:.2f}', end=end)
 
     def test(self, environment: Env) -> None:
+        self.agent.eval()
         self.run_episode(environment, stage='test')
 
     def run_episode(self, environment: Env, stage: Literal['fit', 'test']) -> None:
